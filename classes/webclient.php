5e7eb80a7c17044a061e127f903b473301e1c918
Syrus Akbary <dimension.net@gmail.com>
2010-03-09 21:05:51 +0100

    Added Webclient Class and one example

---
+++ classes/webclient.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Webclient extends Syrus_Webclient {
+}
+/*Example
+var_dump(
+	Webclient::factory()
+	->url('https://www.cia.gov/?primero=si')
+	->get(array('p'=>'uno'))
+	->post(array('p'=>'uno'))
+	->exec()
+);
+*/
